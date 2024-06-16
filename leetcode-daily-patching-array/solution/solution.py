b657f0b8d2eb462e9b7de8cf0a5c90f9b4e0aec9
Scott van Leeuwen <78891914+svanlee@users.noreply.github.com>
2024-06-16 05:53:56 -0400

    init commit

---
+++ leetcode-daily-patching-array/solution/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minPatches(self, nums: List[int], n: int) -> int:
+        miss, i, patches = 1, 0, 0
+        while miss <= n:
+            if i < len(nums) and nums[i] <= miss:
+                miss += nums[i]
+                i += 1
+            else:
+                miss += miss
+                patches += 1
+        return patches
